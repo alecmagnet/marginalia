4aff2e85a6ea08629680e73f4dbcb2891a2374cf
Alec Magnet <amagnet@gmail.com>
2021-10-05 19:14:29 -0400

    miu text container in LitTextShow

--- client/src/features/litTexts/LitTextShow.js
+++ client/src/features/litTexts/LitTextShow.js
@@ -2,6 +2,7 @@ import parse from 'html-react-parser'
 import { useParams } from 'react-router-dom'
 import { useEffect } from "react"
 import { useSelector, useDispatch } from 'react-redux'
+import { Grid, Paper, Typography } from '@mui/material'
 import CommentsList from '../comments/CommentsList'
 import { fetchLitTextById } from './showTextSlice';
 
@@ -30,25 +31,68 @@ export default function LitTextShow() {
 
 	if (litTextState.status === "idle" && litTextState.entities.length > 0) {
 		return (
+    <Grid 
+			container 
+			justifyContent="Center"	
+			alignItems="center"
+		>
+			<Grid 
+				item xs={9}
+			>
+				<Paper 
+					elevation={9} 
+					sx={{ p:3, m: 3, backgroundColor: "#fffaf5" }}
+				>
+					<Typography variant="h5" sx={{ textAlign:"center" }}><b>{litText.title}</b></Typography>
+					{/* <h3>{title}</h3> */}
+					<Typography variant="subtitle1" sx={{ textAlign:"center" }}>{litText.author}</Typography>
+					<Typography variant="body2" sx={{ textAlign:"center" }}><em>{litText.pubdate}</em></Typography>
+					<Typography variant="body1" sx={{ p:3, display:"flex", justifyContent:"center" }}>
+						<div style={{ margin:"0 auto" }}>
+						{parsedContent}</div>
+						</Typography>
+
+				</Paper>
+			</Grid>
 			<div style={{padding: 50}} >
-				<h3>{litText.title}</h3>
+				{/* <h3>{litText.title}</h3>
 				<h4>{litText.author}</h4>
 				<div>{parsedContent}</div>
-				<p><em>{litText.pubdate}</em></p>
+				<p><em>{litText.pubdate}</em></p> */}
 				{/* <a id="comments"> */}
 					<CommentsList 
 						litTextId={litText.id} 
 					/>
 				{/* </a>  */}
 			</div>
+		</Grid>
 		)
-	} else if (litTextState.status === "error") {
-		return (
-			<h1>We're sorry. There's been an error</h1>
-		)
+	// } else if ((litTextState.entities.length === 0 && litTextState.status === "idle") ||litTextState.entities[0].errors) {
+	// 	return (
+	// 		<div className="centered-in-window" >
+	// 			<h1>We're sorry. There's been an error</h1>
+	// 		</div>
+	// 	)
 	} else {
 		return (
-			<h1>Loading...</h1>
+			<div className="centered-in-window" >
+				<h1>Loading...</h1>
+			</div>
 		)
 	}
 }
+
+	// if (litTextState.status === "idle" && litTextState.entities.length > 0) {
+	// 	return (
+	// 		<div style={{padding: 50}} >
+	// 			<h3>{litText.title}</h3>
+	// 			<h4>{litText.author}</h4>
+	// 			<div>{parsedContent}</div>
+	// 			<p><em>{litText.pubdate}</em></p>
+	// 			{/* <a id="comments"> */}
+	// 				<CommentsList 
+	// 					litTextId={litText.id} 
+	// 				/>
+	// 			{/* </a>  */}
+	// 		</div>
+	// 	)
