9a3289ddaf9036b8f257746dd5ec734a09c7fb95
Alec Magnet <amagnet@gmail.com>
2021-10-27 20:39:50 -0400

    fixed unique key warning AND added lots of unneeded keys

--- client/src/features/homepage/AboutPage.js
+++ client/src/features/homepage/AboutPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, Fragment } from 'react'
 import { useHistory, Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { HashLink } from 'react-router-hash-link'
@@ -6,7 +6,7 @@ import { Typography, Grid, Box, Stack, Button, Avatar } from '@mui/material'
 import HpButtons from './HpButtons'
 
 export default function AboutPage() {
-	const userState = useSelector((state) => state.user)
+	const userState = useSelector(state => state.user)
   const user = userState.entities.length > 0 && userState.errors.length === 0 ? userState.entities[0] : null
 	const history = useHistory()
 
@@ -78,12 +78,13 @@ export default function AboutPage() {
 
 
 	const buttonArr = ["What Is It?", "How Do I Use It?", "How Was It Made?", "Who Made It?"]
+	
 	const buttons = <Stack spacing={2} direction="row" sx={{ mb: 1 }}>
 		{buttonArr.map(item => {
 			const itemHash = `/about#${item.slice(0, -1).toLowerCase().replaceAll(" ", "-")}`
 			return (
-				<HashLink smooth to={itemHash} key={buttonArr.indexOf(item)}> 
-					<Button variant="text" id={item} key={buttonArr.indexOf(item)}>{item}</Button>
+				<HashLink key={item} smooth to={itemHash}> 
+					<Button variant="text" id={item}>{item}</Button>
 				</HashLink>
 			)
 		})}
@@ -92,11 +93,11 @@ export default function AboutPage() {
 	const userStoryItems = [
 		["Securely sign up, log in, log out, and remain authorized on refresh"],
 		["Upload poems, stories, and other literary texts"],
-		["Edit texts they uploaded", [<span>Or, if the user is a &ldquo;librarian&rdquo; user-type, edit any uploaded text</span>]],
+		["Edit texts they uploaded", [<span key={1}>Or, if the user is a &ldquo;librarian&rdquo; user-type, edit any uploaded text</span>]],
 		["Browse, search, filter, and read those texts"],
 		["Post comments on texts and replies to other comments"],
-		["Edit and delete their own comments", [<span>If a comment has replies or is a reply, deleting it will leave a ghost that says “comment was deleted” rather than simply vanishing</span>]],
-		[<span>Assign their comments multiple comment types, such “question” or “critique”</span>, ["Add and remove comment types from their comments"]],
+		["Edit and delete their own comments", [<span key={2}>If a comment has replies or is a reply, deleting it will leave a ghost that says “comment was deleted” rather than simply vanishing</span>]],
+		[<span key={3}>Assign their comments multiple comment types, such “question” or “critique”</span>, ["Add and remove comment types from their comments"]],
 		["Browse, search, and filter all comments on a particular text"],
 		["Browse, search, and filter other users’ profiles"],
 		["Edit their own profile"],
@@ -104,15 +105,15 @@ export default function AboutPage() {
 	]
 
 	const storyList = arr => arr.map(item => {
-		const itemLi = itemElement => <li><Typography variant="body1" component="span">{itemElement}</Typography></li>
+		const itemLi = itemElement => <li key={itemElement}><Typography key={`${itemElement}1`} variant="body1" component="span">{itemElement}</Typography></li>
 		if (item.length === 1) {
 			return itemLi(item[0]) 
 		} else {
 			return (
-			<>
+			<Fragment key={item}>
 				{itemLi(item[0])}
-				{item[1].map(item => <ul>{itemLi(item)}</ul>)}
-			</>
+				<ul>{item[1].map(item => itemLi(item))}</ul>
+			</Fragment>
 			)
 		}
 	})
@@ -135,7 +136,7 @@ export default function AboutPage() {
 
 	const linkList = (arr) => arr.map(item => {
 		return(
-			<span key={arr.indexOf(item)}>{item === arr[arr.length - 1] ? "and " : null}
+			<span key={item[0]}>{item === arr[arr.length - 1] ? "and " : null}
 				<a
 				href={item[1]}
 				rel="noreferrer" target="_blank" 
@@ -147,18 +148,18 @@ export default function AboutPage() {
 
 	const whatIsMargArr = [
 		"Marginalia is a social reading and annotation site where people can have conversations with one another about literary works they share and read.",
-		<span>The word <em>marginalia</em> refers to the notes and markings that readers make in the margins of page. These notes leave traces of a reader&rsquo;s progress through a text. They also provide a way for readers to communicate with one another, sometimes from across centuries. The first poem in our database – appropriately, &ldquo;<Link to="/texts/1" style={{ color: "#455a64" }}>Marginalia</Link>&rdquo; by Billy Collins – conveys how moving these moments of unexpected contact with another reader can be.</span>,
+		<span key={4}>The word <em>marginalia</em> refers to the notes and markings that readers make in the margins of page. These notes leave traces of a reader&rsquo;s progress through a text. They also provide a way for readers to communicate with one another, sometimes from across centuries. The first poem in our database – appropriately, &ldquo;<Link to="/texts/1" style={{ color: "#455a64" }}>Marginalia</Link>&rdquo; by Billy Collins – conveys how moving these moments of unexpected contact with another reader can be.</span>,
 		"This app, Marginalia, seeks to restore the social dimension of reading by providing a venue for readers to speak with one another through the comments they leave on literary texts."
 	]
 
 	const whatCanUserArr = [
-		<span>
+		<span key={1}>
 			A user of Marginalia can:
 		</span>
 	]
 
 	const howWasItMadeArr = [
-		<span>
+		<span key={5}>
 			Marginalia was my final project for software engineering bootcamp at <a 
 				href="https://flatironschool.com/campus-and-online-coding-bootcamp/" 
 				rel="noreferrer" target="_blank" 
@@ -169,10 +170,10 @@ export default function AboutPage() {
 				style={{ color: "#455a64"}}
 			>GitHub</a>.
 		</span>,
-		<span>
+		<span key={6}>
 			I coded Marginalia using JS, React, Redux, Ruby, and Rails, along with several other dependencies, including {linkList(dependencyLinks)}.
 		</span>,
-		<span>
+		<span key={7}>
 			Through trial and a <em>lot</em> of error, I learned a crucial lesson about coding, which I plan to apply from here on out: <b>
 				Plan for modularity by making your code as straightforward and standardized as possible!
 			</b>
@@ -184,11 +185,11 @@ export default function AboutPage() {
 
 	const whoMadeItArr = [
 		"I did. Hello! My name is Alec Magnet. I graduate from coding bootcamp in October 2021 after thirteen years teaching literature and writing at the City College of New York, Marymount Manhattan College, and elsewhere. Now I'm looking for a full-stack software engineering position position where I can put the talents and enthusiasm I developed as an educator to use building tools that can use to learn, grow, and flourish.",
-		<span>Check me out on {linkList(myProfiles)}.</span>
+		<span key={8}>Check me out on {linkList(myProfiles)}.</span>
 	]
 
-	const contentGrafs = (arr) => arr.map(item => 
-		<Typography key={arr.indexOf(item)} variant="body1" sx={{ pt: 2 }} >
+	const contentGrafs = (arr) => arr.map((item, key) => 
+		<Typography key={key} variant="body1" sx={{ pt: 2 }} >
 			{item} 
 		</Typography>		
 	)
