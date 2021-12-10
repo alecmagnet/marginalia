26d9da616ff2c6bf687f41b39e2fab08f8cc6541
Alec Magnet <amagnet@gmail.com>
2021-12-10 09:59:19 -0500

    added back & forward buttons

--- client/src/features/maCanvas/MovingAvgContainer.js
+++ client/src/features/maCanvas/MovingAvgContainer.js
@@ -1,19 +1,23 @@
 import { useState, useEffect, useCallback } from 'react'
-import { Grid, Typography, } from '@mui/material'
+import { Grid, Typography, IconButton } from '@mui/material'
+// import { ArrowBackIosIcon, ArrowForwardIosIcon } from '@mui/icons-material'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import { defaultPricesObj } from './defaultPrices'
 import MovingAvgCanvas from './MovingAvgCanvas'
 
 export default function MovingAvgContainer() {
 	const [prices, setPrices] = useState([])
 	const [error, setError] = useState(false)
+	const [daysAgo, setDaysAgo] = useState(0)
 
 	const makeClosingPricesArr = useCallback((obj1, bool) => {
 		let result = []
-		let obj = obj1["Time Series (Daily)"]
+		let obj = obj1['Time Series (Daily)']
 		for (const property in obj) {
 			result.push({
 				date: property, 
-				price: Number(obj[property]["4. close"])
+				price: Number(obj[property]['4. close'])
 			})
 		}
 		setError(bool)
@@ -42,27 +46,45 @@ export default function MovingAvgContainer() {
 		fetchPrices()
 	}, [fetchPrices])
 
+	const handleDayClick = (direction) => {
+		const addend = direction === "back" ? 1 : -1
+		setDaysAgo(prev => prev + addend)
+	}
 
 	return (
-		<Grid container justifyContent="center" spacing={2}>
+		<Grid container justifyContent='center' spacing={2}>
 			<Grid item xs={12}>
-				<Typography variant="h3" textAlign="center" sx={{ mt: 4 }}>
+				<Typography variant='h3' textAlign='center' sx={{ mt: 4 }}>
 					IBM Closing Prices
 				</Typography>
-
-				{error ? 
-					<Grid item xs={12}>
-						<Typography variant="h5" textAlign="center">
-							We could not fetch the latest data 
-						</Typography>
-						<Typography variant="subtitle1" textAlign="center">	
-							We’re sorry. Please try again in 5 minutes. Until then, here is our most recent stored data
-						</Typography>
-					</Grid>
-				: null}
-
 			</Grid>
-			{prices.length > 0 ? <MovingAvgCanvas prices={prices} /> : null}
+			<Grid item xs={12} textAlign='center' >
+				<IconButton
+					disabled={daysAgo >= 80}
+					onClick={() => handleDayClick("back")}
+				>
+					<ArrowBackIosNewIcon />
+				</IconButton>
+				<IconButton
+					disabled={daysAgo === 0}
+					onClick={() => handleDayClick("forward")}
+				>
+					<ArrowForwardIosIcon />
+				</IconButton>
+			</Grid>
+
+			{error ? 
+				<Grid item xs={12}>
+					<Typography variant='h5' textAlign='center'>
+						We could not fetch the latest data 
+					</Typography>
+					<Typography variant='subtitle1' textAlign='center'>	
+						We’re sorry. Please try again in 5 minutes. Until then, here is our most recent stored data
+					</Typography>
+				</Grid>
+			: null}
+
+			{prices.length > 0 ? <MovingAvgCanvas prices={prices} daysAgo={daysAgo} /> : null}
 		</Grid>
 	)
 }
